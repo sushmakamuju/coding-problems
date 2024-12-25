24f3553eae6be25ccc725e4cdc3b62581c9a52b5
unknown <sushmakamuju333@gmail.com>
2024-12-25 18:45:52 +0530

    add solution for two sum problem

---
+++ two-sum/README.md
@@ -0,0 +1,12 @@
+# Two Sum Problem
+
+## Problem Link
+[LeetCode - Two Sum](https://leetcode.com/problems/two-sum)
+
+## Approach
+- Use a hash map to store numbers and their indices.
+- For each number, check if its complement (target - number) exists in the map.
+
+## Time Complexity
+- Time: O(n)
+- Space: O(n)
---
+++ two-sum/solution.cpp
@@ -0,0 +1,15 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> map;
+    for (int i = 0; i < nums.size(); ++i) {
+        int complement = target - nums[i];
+        if (map.count(complement)) {
+            return {map[complement], i};
+        }
+        map[nums[i]] = i;
+    }
+    return {};
+}
